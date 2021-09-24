f2387ebf1f55651f94adf5c04fda18e169a4ec98
trabajosptoyectos <ADMISIONAC35@GMAIL.COM>
2021-09-24 19:23:31 +0000

    updt

--- .learn/resets/11-Create-a-new-function/app.js
+++ .learn/resets/11-Create-a-new-function/app.js
@@ -1,10 +1,3 @@
 /* This exercise is almost identical to 15-Random numbers - consider merging them here and using another concept for 15? */
 
 // Declare your function here
-
-function generateRamdom(){
-
-    return Math.random(10);
-}
-
-console.log(generateRamdom());
--- exercises/12-Your-first-if/app.js
+++ exercises/12-Your-first-if/app.js
@@ -1,3 +1,19 @@
 var total = prompt('How many km are left to go?');
 
 // Your code below:
+if(parseFloat(total)> 100){
+
+    console.log("We still have a bit of driving left to go");
+
+
+}
+
+else if (parseFloat(total)<= 100 && parseFloat(total)> 50) {
+    console.log("We'll be there in 5 minutes");
+}
+
+else{
+
+    console.log("I'm parking. I'll see you right now");
+
+}
